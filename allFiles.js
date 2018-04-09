13ca03d415359722bc15cbfb7c0d181a07f5b04f
gt <guangtuan.zhuang@yiwill.com>
2018-04-09 12:05:36 +0800

    add readme

--- allFiles.js
+++ allFiles.js
@@ -1,7 +1,6 @@
 const walk = require('walk');
 const path = require('path');
 
-// 递归文件夹找到 png 结尾的文件，放到一个数组里
 module.exports = root => new Promise((resolve, reject) => {
     const walker = walk.walk(root, { followLinks: false });
     let files = [];
